e37aa285c69fab6ff5e3c0cbf946349363cdb5c3
Souvik Bhattacharyya <souvikshanku@gmail.com>
2023-05-30 00:48:50 +0530

    Solve part 1

--- src/p3.rs
+++ src/p3.rs
@@ -1,24 +1,43 @@
-// use std::collections::HashMap;
 use std::fs;
 
 
 pub fn solve_p3() {
-
     let file_path = "./inputs/p3.txt";
 
     let contents = fs::read_to_string(file_path)
         .expect("Should have been able to read the file");
     let rucksacks: Vec<_> = contents.split("\n").collect();
 
-    // println!("{:?}", rucksacks);
-    for rucksack in &rucksacks[0..1]{
-        check_common_item(rucksack);
+    let mut sum: usize = 0;
+    for rucksack in &rucksacks{
+        sum += check_common_item(rucksack);
     }
+
+    println!("{}", sum);
 }
 
 
-fn check_common_item(rucksack: &str) {
+fn get_value_from_alpha(alpha: &str) -> usize {
+    let small: Vec<_> = "abcdefghijklmnopqrstuvwxyz".split("").collect();
+    let caps: Vec<_> = "ABCDEFGHIJKLMNOPQRSTUVWXYZ".split("").collect();
 
+    for idx in 0..small.len() {
+        if alpha == small[idx] {
+            return idx
+        }
+    }
+
+    for idx in 0..caps.len() {
+        if alpha == caps[idx] {
+            return idx + 26
+        }
+    }
+
+    return 0
+
+}
+
+fn check_common_item(rucksack: &str) -> usize {
     let rucksack: Vec<char> = rucksack.chars().collect();
     let length = rucksack.len();
     let half_length = length/ 2;
@@ -30,10 +49,18 @@ fn check_common_item(rucksack: &str) {
         first_half[i] = rucksack[i];
     }
 
-    // for j in half_length..length {
-    //     second_half[j] = rucksack[j];
-    // }
+    for i in 0..half_length {
+        second_half[i] = rucksack[half_length + i];
+    }
+    
+    let mut result: usize = 0;
 
-    println!("{:?}", first_half);
+    for i in 0..half_length {
+        if second_half.contains(&first_half[i]) {
+            result = get_value_from_alpha(&first_half[i].to_string());
+            break;
+        }
+    }
+
+    return result;
 }
- 
