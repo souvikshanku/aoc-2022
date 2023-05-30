019182ae959a35686d5cae0f0407df87f21e4379
Souvik Bhattacharyya <souvikshanku@gmail.com>
2023-05-31 00:02:29 +0530

    Day 4 was kinda easy...

--- src/p4.rs
+++ src/p4.rs
@@ -8,7 +8,8 @@ pub fn solve_p4() {
         .expect("Should have been able to read the file");
     let assignments: Vec<_> = contents.split("\n").collect();
 
-    let mut count: usize = 0;
+    let mut count1: usize = 0;
+    let mut count2: usize = 0;
 
     for assignment in assignments {
         let a_vec: Vec<_> = assignment.split(",").collect();
@@ -25,11 +26,18 @@ pub fn solve_p4() {
         let b2 = b[1].parse::<i32>().unwrap();
 
         if a1 <= b1 && a2 >= b2 {
-            count += 1;
+            count1 += 1;
         } else if a1 >= b1 && a2 <= b2 {
-            count += 1;
+            count1 += 1;
+        }
+
+        if a1 <= b1 && a2 >= b1 {
+            count2 += 1;
+        } else if b1 <= a1 && b2 >= a1 {
+            count2 += 1;
         }
     }
 
-    println!("{:?}", count);
+    println!("{:?}", count1);
+    println!("{:?}", count2);
 }
