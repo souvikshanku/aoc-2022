8f49880fd18323335b048bcf7cddb4f6c5075976
Souvik Bhattacharyya <souvikshanku@gmail.com>
2023-05-30 23:44:36 +0530

    day 4 part 1 done

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,17 @@
-mod p1;
-mod p2;
-mod p3;
-mod p3_2;
+// mod p1;
+// mod p2;
+// mod p3;
+// mod p3_2;
+mod p4;
 
 fn main() {
-    println!("-----------Day 1-----------");
-    p1::solve_p1();
-    println!("-----------Day 2-----------");
-    p2::solve_p2();
-    println!("-----------Day 3-----------");
-    p3::solve_p3();
-    p3_2::solve_p3();
+    // println!("-----------Day 1-----------");
+    // p1::solve_p1();
+    // println!("-----------Day 2-----------");
+    // p2::solve_p2();
+    // println!("-----------Day 3-----------");
+    // p3::solve_p3();
+    // p3_2::solve_p3();
+
+    p4::solve_p4();
 }
---
+++ src/p4.rs
@@ -0,0 +1,35 @@
+use std::fs;
+
+
+pub fn solve_p4() {
+    let file_path = "./inputs/p4.txt";
+
+    let contents = fs::read_to_string(file_path)
+        .expect("Should have been able to read the file");
+    let assignments: Vec<_> = contents.split("\n").collect();
+
+    let mut count: usize = 0;
+
+    for assignment in assignments {
+        let a_vec: Vec<_> = assignment.split(",").collect();
+
+        let a = a_vec[0];
+        let b = a_vec[1];
+
+        let a: Vec<_> = a.split("-").collect();
+        let b: Vec<_> = b.split("-").collect();
+
+        let a1 = a[0].parse::<i32>().unwrap();
+        let a2 = a[1].parse::<i32>().unwrap();
+        let b1 = b[0].parse::<i32>().unwrap();
+        let b2 = b[1].parse::<i32>().unwrap();
+
+        if a1 <= b1 && a2 >= b2 {
+            count += 1;
+        } else if a1 >= b1 && a2 <= b2 {
+            count += 1;
+        }
+    }
+
+    println!("{:?}", count);
+}
